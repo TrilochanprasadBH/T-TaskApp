233b533815300abec8c298a319917fcfa8d21956
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-09-20 21:45:44 +0530

    setup ts tasks app revised ts

---
+++ tasks/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  
+  
+  
+  return (
+    <div className="App">
+     <p>T-Tasks App</p>
+    </div>
+  );
+}
+
+export default App;
